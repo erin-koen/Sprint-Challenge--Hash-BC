12c83f5d0904d60e82b6f1b8b2a50824152ddba4
Erin Koen <erin.koen@gmail.com>
2019-07-12 12:52:57 -0400

    ex2.py passing tests

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -15,9 +15,25 @@ class Ticket:
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
+    print('WHAT IS ROUTE', type(route))
 
     """
     YOUR CODE HERE
+    Take a list of Tickets
     """
+    #  Get the tickets into the HT, key = source and val = destination
+    for i in tickets:
+        if i.source is "NONE":  # i know this is risky but .upper() was throwing an error and i need to move to blockchain
+            hash_table_insert(hashtable, "home", i.destination)
+        else:
+            hash_table_insert(hashtable, i.source, i.destination)
+
+    start = "home"
+
+    for i in range(0, len(route)):
+        dest = hash_table_retrieve(hashtable, start)
+
+        route[i] = dest
+        start = dest
 
     return route
